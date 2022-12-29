682030f035922d3b635e115de0ee682a85295565
Asus ExpertBook <sudaratto65@nu.ac.th>
2022-12-29 16:49:48 +0700

    Midterm part 1 GUI

---
+++ scr/app/comm_mqtt.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2022 Sudarat Tokampang <sudaratto65@nu.ac.th>
+
+"""
+communicate with the HIVEMQ broker to receive
+sensor data,using the paho matt library
+"""
+
+__author__ = "Sudarat Tokampang"
+
+import paho.mqtt.client as mqtt
+import main_IoT
+
+import time
+
+# installed library
+import paho.mqtt.client as mqtt
+
+HIVEMQTT_PORT = 1883  # CONSTANT
+HIVEMQTT_BROKER = "broker.hivemq.com"
+PUBLIST_TOPIC = "Naresuan/Sudarat"
+SUBSCRIBE_TOPIC = "Naresuan/+"
+
+
+class MQTTConn:
+    """
+    Use the paho library to connect to the HIVE MQ mqtt broker
+
+    Attributes
+        root(main_gui.SensorUI): root user interface app
+        client (mqtt.Client): paho client for mqtt communication
+    """
+    def __init__(self, root: main_IoT.SensorUI):
+        self.root = root
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connection
+        #self.client.on_subscribe = self.on_subscription
+        self.client.on_message = self.on_message
+        self.client.connect(HIVEMQTT_BROKER, HIVEMQTT_PORT)
+        self.client.loop_start()
+
+    def publish(self, message):
+        """
+        Send a message to the HIVE MQ broker using the PUBLISH_TOPIC
+        Args:
+            message(str): massage to send
+
+        Returns:
+
+        """
+        self.client.publish(PUBLIST_TOPIC, message)
+
+    def on_connection(self, *args):
+        """call back for when mqtt connects to the broken
+        and prints out acknowledgment and subscribes"""
+        print("Connected")
+        self.client.subscribe(SUBSCRIBE_TOPIC)
+
+
+    def on_message(self, client, user_data, msg: mqtt.MQTTMessage):
+        """
+        Callback when receiving message
+        Args:
+            client:
+            user_data:
+            msg(mqtt.MQTTMessage): message received
+
+        """
+        print("got message: ", msg.payload)
+        print("from topic", msg.topic)
+        name = msg .topic .split('/')[-1]
+        print("message from: ", name)
+        if msg.payload == b"On":
+            sensor_state = True
+        else:
+            sensor_state = False
+        #self.root.change_status(name,
+        #                        sensor_state)
+
+
+if __name__ == '__main__':
+    test_client = MQTTConn(None)
+    while True:
+        test_client.publish("hello_this is MOOK")
+        time.sleep(10)
+
---
+++ scr/app/main_IoT.py
@@ -0,0 +1,74 @@
+# Copyright (c) 2022 Sudarat Tokampang <sudaratto65@nu.ac.th>
+
+""" Document string (Doc String)
+A graphical user interface to observe sensor data
+from a remote sensor through a matt communication channel
+"""
+
+__author__ = "Sudarat Tokampang"
+
+# standard libra
+import tkinter as tk
+import comm_mqtt  # MQTT
+
+NAMES = ["Pick Rock", "Pick Scissor", "Pick Paper"]
+
+
+class SensorUI(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+        self.comm = comm_mqtt.MQTTConn(self)  # MQTT
+
+        self.running = False
+        self.button1 = tk.Button(self, text="Pick Rock",
+                                command=self.button1_click)
+        self.button1.pack(side=tk.TOP)
+
+        self.button2 = tk.Button(self, text="Pick Scissor ",
+                                command=self.button2_click)
+        self.button2.pack(side=tk.TOP)
+
+        self.button3 = tk.Button(self, text="Pick Paper ",
+                                command=self.button3_click)
+        self.button3.pack(side=tk.TOP)
+
+        self.label = tk.Label(text="Your Pick")
+        self.label.pack(side=tk.TOP)
+
+
+    def button1_click(self):
+        """
+        Toggle the stat of the sensor True -> False or False -> True,
+        update the text of the run button, update the local StatusButton,
+        and send the proper mqtt message
+
+        """
+        self.label.config(text="You pick Rock")
+
+        msg = "Sudarat picked Rock"  # MQTT
+        self.comm.publish(msg)    # MQTT
+
+
+
+    def button2_click(self):
+        self.label.config(text="You pick Scissor")
+        msg = "Sudarat picked Scissor" # MQTT
+        self.comm.publish(msg)  # MQTT
+
+
+
+    def button3_click(self):
+        self.label.config(text="You pick Paper")
+        msg = "Sudarat picked Paper"  # MQTT
+        self.comm.publish(msg)  # MQTT
+
+if __name__ == "__main__":
+    app = SensorUI()   # application a class ot tkinter.Tk
+    # geometry is a method of the tkinter.Tk class that
+    # sets the size of the app window. It takes a
+    # string as on argument
+    app.geometry("400x400")
+    app.title('Rock Paper Scissor IoT game')
+    app.mainloop()  # mainloop is method of tkinter.Tk
+    # methods are function of classes
